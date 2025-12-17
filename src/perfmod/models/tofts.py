7a037b53ac30ac13ee2d8b11e42961e42012062f
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2025-12-17 12:46:54 +0100

    Initial release.

--- src/perfmod/models/tofts.py
+++ src/perfmod/models/tofts.py
@@ -3,7 +3,7 @@ from scipy import ndimage
 from . import default_parameters
 
 
-def make_tofts_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
+def make_tofts(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
 
     def tofts(t, *b):
         """h = tofts(t, b), where b = [ktrans ve delay]
@@ -12,18 +12,13 @@ def make_tofts_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
 
         # ktrans=b[0]: ktrans
         # ve=b[1]: EES volume
-        # delay=b[2]: delay in AIF arrival
         """
 
-        ktrans, ve, delay = b
-        dt = t[2] - t[1]
-        aif_shifted = ndimage.shift(aif_value, delay / dt, mode='nearest')
-        print('tofts: ktrans {} ve {} td {}'.format(ktrans, ve, delay))
-        aif_shifted = aif_value
+        ktrans, ve = b
 
         # kep = ktrans/ve
         h = ktrans * np.exp(-ktrans/ve * t)
-        h = np.convolve(h, aif_shifted)[:t.size]
+        h = np.convolve(h, aif_value)[:t.size]
         return h
 
     # Set defaults
@@ -31,14 +26,13 @@ def make_tofts_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
     prm = default_parameters()
     # prm['x_scale'] = np.array([0.1, 1, 1, 1, 1, 1])  # [10 1 10 1 10];
     prm['x_scale'] = None
-    prm['lower_bounds'] = np.array([0, 0, -1.5])
-    prm['upper_bounds'] = np.array([np.inf, np.inf, 1.5])
+    prm['lower_bounds'] = np.array([0, 0])
+    prm['upper_bounds'] = np.array([np.inf, np.inf])
 
     # optimization parameters, initialization
-    b0 = {'ktrans': 0.2, 've': 0.2, 'delay': 0.}
-    prm['parameters'] = ['ktrans', 've', 'delay']
-    prm['units'] = {'ktrans': 'ml/ml/min', 've': 'ml/ml/min',
-                    'delay': 'min'};
+    b0 = {'ktrans': 0.2, 've': 0.2}
+    prm['parameters'] = ['ktrans', 've']
+    prm['units'] = {'ktrans': 'ml/ml/min', 've': 'ml/ml/min'}
 
     # Apply user-provided parameters
     prm = prm | prmin
