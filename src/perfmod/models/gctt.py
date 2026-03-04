889900c83e072c63716da6febb30fdf5df2ca700
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2026-03-04 11:38:02 +0100

    Initial release.

--- src/perfmod/models/gctt.py
+++ src/perfmod/models/gctt.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import ndimage
 import scipy.special as sc
 from . import default_parameters
+from ..aif.parker import parker
 
 
 def make_gctt(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
@@ -43,13 +44,15 @@ def make_gctt(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
     prm = default_parameters()
     # prm['x_scale'] = np.array([0.1, 1, 1, 1, 1, 1])  # [10 1 10 1 10];
     prm['x_scale'] = None
+    # prm['lower_bounds'] = np.array([0, 0, 1e-9, 0, 0])
+    # prm['upper_bounds'] = np.array([np.inf, 1, np.inf, np.inf, 1])
     prm['lower_bounds'] = np.array([0, 0, 1e-9, 0, 0])
-    prm['upper_bounds'] = np.array([np.inf, 1, np.inf, np.inf, 1])
+    prm['upper_bounds'] = np.array([np.inf, 1, 1, np.inf, 1])
 
     # optimization parameters, initialization
-    b0 = {'F': 0.1, 'E': 0.2, 've': 0.2, 'Tc': 10./60, 'alphainv': 0.6}
+    b0 = {'F': 1.0, 'E': 0.5, 've': 0.5, 'Tc': 0.1, 'alphainv': 0.2}
     prm['parameters'] = ['F', 'E', 've', 'Tc', 'alphainv']
-    prm['units'] = {'F': 'ml/ml/min', 'E': None, 've': 'ml/ml/min', 'Tc': 'min',
+    prm['units'] = {'F': 'ml/ml/s', 'E': None, 've': None, 'Tc': 'min',
                     'alphainv': None}
 
     # Apply user-provided parameters
@@ -79,12 +82,16 @@ def make_gctt_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
         # t = t.copy() + delay
         dt = t[2] - t[1]
         aif_shifted = ndimage.shift(aif_value, delay / dt, mode='nearest')
-        print('gctt: F {} E {} ve {} Tc {} a-1 {} td {}'.format(F, E, ve, Tc, alphainv, delay))  # , end='')
+        # aif_shifted = getParker(aif_value, 1/norm_coeff, delay, t)
+        aif_shifted = parker(parker_parameters, len(t), t - delay)
+        aif_shifted /= np.max(aif_shifted)
+        # print('gctt: F {} E {} ve {} Tc {} a-1 {} td {}'.format(F, E, ve, Tc, alphainv, delay))  # , end='')
 
         alpha = 1 / alphainv
         tau = Tc / alpha
         kep = (E * F) / ve
         if (1 / tau - kep) < 0:
+            return np.full(t.shape, 1e8)
             raise ValueError("gammaincc(.., 1/tau - kep) is negative")
 
         Rv = sc.gammaincc(alpha, t / tau)
@@ -92,7 +99,7 @@ def make_gctt_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
              (1 - sc.gammaincc(alpha, (1 / tau - kep) * t))
 
         R = Rv + Rp
-        h = F * np.convolve(R, aif_shifted)[:t.size]
+        h = F * np.convolve(R, aif_shifted)[:t.size] * dt
 
         # Hv = np.convolve(Rv * F, aif_value)  # vascular part of IRF
         # Hp = np.convolve(Rp * F, aif_value)  # parenchymal part of IRF
@@ -104,19 +111,24 @@ def make_gctt_delay(aif_value, b0in, prmin) -> tuple[callable, dict, dict]:
     prm = default_parameters()
     # prm['x_scale'] = np.array([0.1, 1, 1, 1, 1, 1])  # [10 1 10 1 10];
     prm['x_scale'] = None
+    # prm['lower_bounds'] = np.array([0, 0, 1e-9, 0, 0, -1.5])  # -10])
+    # prm['upper_bounds'] = np.array([np.inf, 1, np.inf, np.inf, 1, 1.5])  # 30])
     prm['lower_bounds'] = np.array([0, 0, 1e-9, 0, 0, -1.5])  # -10])
-    prm['upper_bounds'] = np.array([np.inf, 1, np.inf, np.inf, 1, 1.5])  # 30])
+    prm['upper_bounds'] = np.array([np.inf, 1, 1, np.inf, 1, 1.5])  # 30])
+    prm['diff_step'] = 0.1
     # info.perfan.params = {'F [ml/(ml min)]', 'E [-]', 've [ml/ml]', 'Tc [min]', 'alphainv [-]', 'BAT [min]'};
 
     # optimization parameters, initialization
     # F [ml/(ml min)], E [-], ve [ml/ml], Tc [min], alphainv [-], BAT [min]};
-    b0 = {'F': 0.1, 'E': 0.2, 've': 0.2, 'Tc': 10./60, 'alphainv': 0.6, 'delay': 0.}
+    b0 = {'F': 1., 'E': 0.5, 've': 0.5, 'Tc': 0.1, 'alphainv': 0.2, 'delay': 0.}
     prm['parameters'] = ['F', 'E', 've', 'Tc', 'alphainv', 'delay']
-    prm['units'] = {'F': 'ml/ml/min', 'E': None, 've': 'ml/ml/min', 'Tc': 'min',
-                    'alphainv': None, 'delay': 'min'}
+    prm['units'] = {'F': 'ml/ml/s', 'E': None, 've': None, 'Tc': 'min',
+                    'alphainv': None, 'delay': 's'}
 
     # Apply user-provided parameters
     prm = prm | prmin
     b0 = b0 | b0in
+
+    parker_parameters = prm['parker_parameters']
 
     return gctt, b0, prm
